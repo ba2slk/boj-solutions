9d3c08201f647147c35707e292a93895bc2173ae
ba2slk <0730bss@gmail.com>
2025-07-17 00:38:49 +0900

    Organize directory

---
+++ BOJ/인사성_밝은_곰곰이.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n; cin >> n;
+    int ans = 0;
+    set<string> s;
+
+    
+
+    cout << ans;
+    
+
+    return 0;
+}
